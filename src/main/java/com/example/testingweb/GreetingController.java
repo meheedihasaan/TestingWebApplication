34b00ec834dbae428b935c10deea0f442d00255c
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-01-07 00:09:55 -0800

    Web application is tested

---
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -0,0 +1,20 @@
+package com.example.testingweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+
+    private GreetingService greetingService;
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @RequestMapping("/greeting")
+    public @ResponseBody String greeting() {
+        return greetingService.greet();
+    }
+
+}
