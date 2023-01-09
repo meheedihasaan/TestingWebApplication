7334870bc8ca0a926ca869a5d32ca3dc86f0078b
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-01-08 21:13:07 -0800

    bug fix

--- src/main/java/com/example/testingweb/GreetingController.java
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GreetingController {
 
-    private GreetingService greetingService;
+    private final GreetingService greetingService;
     public GreetingController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
